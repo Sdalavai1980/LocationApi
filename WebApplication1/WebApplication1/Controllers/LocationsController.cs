7adc9f99a35800abda86962abcfa67f2afee2d43
Sdalavai1980 <158824770+Sdalavai1980@users.noreply.github.com>
2024-02-04 18:36:41 +0530

    Add files via upload
    Location API

---
+++ WebApplication1/WebApplication1/Controllers/LocationsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Net.Http;
+
+// System.Web.Http.Common;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private static List<Location> locations = new List<Location>
+    {
+        new Location { Id = 1, Name = "Location1", AvailabilityStartTime = DateTime.Parse("10:00 AM"), AvailabilityEndTime = DateTime.Parse("1:00 PM") },
+        new Location { Id = 2, Name = "Location2", AvailabilityStartTime = DateTime.Parse("9:00 AM"), AvailabilityEndTime = DateTime.Parse("12:00 PM") },
+        // Add more locations as needed
+    };
+        [HttpGet]
+        [Route("api/locations/availability")]
+        public IHttpActionResult GetLocationsWithAvailability()
+        {
+            DateTime startTime = DateTime.Parse("10:00 AM");
+            DateTime endTime = DateTime.Parse("1:00 PM");
+
+            var availableLocations = locations
+                .Where(l => l.AvailabilityStartTime <= startTime && l.AvailabilityEndTime >= endTime)
+                .Select(l => new { l.Id, l.Name })
+                .ToList();
+
+            return Ok(availableLocations);
+        }
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AvailabilityStartTime { get; set; }
+        public DateTime AvailabilityEndTime { get; set; }
+
+    }
+}
